0a1e4c75f20acbbddde653328112cd7d75d2ba97
ChawinGowanit <chawingowanit@gmail.com>
2023-07-08 10:25:01 +0700

    change vscode setting

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,70 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 public class GameController : MonoBehaviour
 {
-    [SerializeField] int currentNPC = 1;
-    [SerializeField] List<int> queue = new List<int>();
-    [SerializeField] List<string> pQueueTier = new List<string>();
-    [SerializeField] List<string> pQueueName = new List<string>();
-    [SerializeField] List<string> skillQueue= new List<string>();
-    [SerializeField] List<bool> reaction =  new List<bool>();
-    void Start()
+  [SerializeField] int currentNPC = 1;
+  [SerializeField] List<int> npc = new List<int>();
+  [SerializeField] List<string> preferenceTier = new List<string>();
+  [SerializeField] List<string> preferenceName = new List<string>();
+  [SerializeField] List<string> npcSkills = new List<string>();
+  [SerializeField] List<bool> reaction = new List<bool>();
+  void Start()
+  {
+    // init NPC ,pref ,and skill
+    randomGameloop();
+    // start first NPC
+    showNPCPref(currentNPC);
+  }
+  void Update()
+  {
+  }
+  public void randomGameloop()
+  {
+    randomNPC();
+    randomPref();
+    randomSkill();
+  }
+  public void randomNPC()
+  {
+    while (npc.Count < 3)
     {
-        randomNPC();
-        randomPref();
-        randomSkill();
-        showNPC(currentNPC);
-    }
-    void Update()
-    {
-        
-    }
-    public void randomNPC(){
-        while (queue.Count < 3) {
-            bool isIn = false;
-            int r = Random.Range(1, 5);
-            foreach (var item in queue)
-            {
-                if (r == item){
-                    isIn = true;
-                }
-            }
-            if (!isIn){
-                queue.Add(r);
-            }
+      bool isIn = false;
+      int r = Random.Range(1, 5);
+      foreach (var item in npc)
+      {
+        if (r == item)
+        {
+          isIn = true;
         }
+      }
+      if (!isIn)
+      {
+        npc.Add(r);
+      }
     }
-    public void randomPref(){
-        for (int i = 0;i <3;i++){
-            var tiers = new string[] {"silver","gold","rainbow"};
-            var names = new string[] {"Miyu","???"};
+  }
+  public void randomPref()
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      var tiers = new string[] { "silver", "gold", "rainbow" };
+      var names = new string[] { "Miyu", "???" };
 
-            string name = names[Random.Range(0, names.Length)];
-            string tier = tiers[Random.Range(0, tiers.Length)];
-            pQueueName.Add(name);
-            pQueueTier.Add(tier);
-        }
+      string name = names[Random.Range(0, names.Length)];
+      string tier = tiers[Random.Range(0, tiers.Length)];
+      preferenceName.Add(name);
+      preferenceTier.Add(tier);
     }
-    public void randomSkill(){
-        for (int i = 0;i <3;i++){
-            var skills = new string[] {"none","lucky"};
-            string skill = skills[Random.Range(0, skills.Length)];
-            skillQueue.Add(skill);
-        }
+  }
+  public void randomSkill()
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      var skills = new string[] { "none", "lucky", "rich" };
+      string skill = skills[Random.Range(0, skills.Length)];
+      npcSkills.Add(skill);
     }
-    public void showNPC(int currentNPC){
-        //show npc pref /skill
-    }
-    public void onCloseShowNPC(){
-        //release ball to start the game
-    }
-    public void showNPCStory(){
-        //show NPC story when button is clicked
-    }
-    public void onFirstBallReach(string prefTier,List<string> prefName){
-        //add NPC reaction
-    }
-    public void showAllReaction(){
-        //show all NPC reaction
-        //finsih game loop
-    }
+  }
+  public void showNPCPref(int currentNPC)
+  {
+    //show npc pref /skill
+  }
+  public void onCloseShowNPC()
+  {
+    //release ball to start the game
+  }
+  public void showNPCStory()
+  {
+    //show NPC story when button is clicked
+  }
+  public void onFirstBallReach(string prefTier, List<string> prefName)
+  {
+    //add NPC reaction
+  }
+  public void showAllReaction()
+  {
+    //show all NPC reaction
+    //finsih game loop
+  }
 }
--- Assets/Scripts/NPCReactionController.cs
+++ Assets/Scripts/NPCReactionController.cs
@@ -4,21 +4,9 @@ using UnityEngine;
 
 public class NPCReactionController : MonoBehaviour
 {
-    [SerializeField] NPCData npc1;
-    [SerializeField] NPCData npc2;
-    [SerializeField] NPCData npc3;
-    [SerializeField] NPCData npc4;
-    [SerializeField] NPCData npc5;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    { 
-        
-    }
+  [SerializeField] NPCData npc1;
+  [SerializeField] NPCData npc2;
+  [SerializeField] NPCData npc3;
+  [SerializeField] NPCData npc4;
+  [SerializeField] NPCData npc5;
 }
