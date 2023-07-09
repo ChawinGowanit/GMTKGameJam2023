1d0fe287f802b4dce8fe8b4bf7932dd28a6d3684
Kirk Pig <kriklomark@gmail.com>
2023-07-09 09:18:11 +0700

    refactor: ChatController

--- Assets/Scripts/ChatController.cs
+++ Assets/Scripts/ChatController.cs
@@ -9,26 +9,14 @@ public class ChatController : MonoBehaviour
   public TextMeshProUGUI nameObj;
   public TextMeshProUGUI textObj;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
-
   // set NPC name to the component
-  public void setChatName(string newName)
+  public void SetChatName(string newName)
   {
     nameObj.text = newName;
   }
 
   // set chat text to the component
-  public void setChatText(string newText)
+  public void SetChatText(string newText)
   {
     textObj.text = newText;
   }
